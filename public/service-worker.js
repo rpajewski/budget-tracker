e051dbb6330eeb288037eb55a079ca6173480b3a
Ricky Bobby <richard.pajewski@gmail.com>
2021-01-23 10:55:44 -0600

    file structure created, manifest and service worker started

---
+++ public/service-worker.js
@@ -0,0 +1,60 @@
+const APP_PREFIX = 'BudgetTracker-'
+const VERSION = 'version_01'
+const CACHE_NAME = APP_PREFIX + VERSION
+
+const FILES_TO_CACHE = [
+    "./index.html",
+    "./css/styles.css",
+    "./icons/icon-192x192.png",
+    "./js/index.js",
+    "./js/idb.js"
+]
+
+// Respond with cached resources
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            // if cache is available, respond with cache
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            // if there are no cache, try fetching request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+})
+
+// Cache resources
+self.addEventListener('install', function (e) {
+    e.waitUntil(
+        caches.open(CACHE_NAME).then(function (cache) {
+            console.log('installing cache : ' + CACHE_NAME)
+            return cache.addAll(FILES_TO_CACHE)
+        })
+    )
+})
+
+// Delete outdated caches
+self.addEventListener('activate', function (e) {
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            // filter out ones that has this app prefix to create keeplist
+            let cacheKeeplist = keyList.filter(function (key) {
+                return key.indexOf(APP_PREFIX)
+            })
+            // add current cache name to keeplist
+            cacheKeeplist.push(CACHE_NAME)
+
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeeplist.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] )
+                    return caches.delete(keyList[i])
+                }
+            }))
+        })
+    )
+})
